ee4f369f16b823e7f12d6b6951568adad6a6e5e4
yousefshref <yb2005at@gmail.com>
2024-08-05 22:00:22 +0300

    extra data

--- api/models.py
+++ api/models.py
@@ -43,6 +43,7 @@ class Certificate(models.Model):
 
 
     data = models.JSONField(null=True, blank=True)
+    extra_data = models.JSONField(null=True, blank=True)
 
     def __str__(self):
         return self.number
